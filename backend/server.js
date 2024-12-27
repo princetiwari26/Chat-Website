08ac3cfac73b3364e35c4c7c45ba5df34e4e27a7
Prince Tiwari <princetiwari.profes@gmail.com>
2024-12-27 23:07:13 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ const experss = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
+const jwt = require("jsonwebtoken");
 const http = require('http')
+const { Server } = require('socket.io')
 const DBConnection = require('./config/dbConnection')
 const userRouter = require('./routers/user.router')
+const chatRouter = require('./routers/chat.router')
 dotenv.config()
 DBConnection()
 
@@ -12,6 +15,14 @@ const app = experss()
 const PORT = process.env.PORT
 
 const server = http.createServer(app)
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+})
+
 
 app.use(experss.json())
 app.use(cookieParser())
@@ -22,8 +33,35 @@ const corsOptions = {
 };
 app.use(cors(corsOptions))
 
-app.use('/api/auth/user', userRouter)
 
-app.listen(PORT, () => {
+app.get("/api/auth/verify", (req, res) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "No token found" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
+    req.id = decoded.userId
+    // console.log(req.id)
+    return res.status(200).json({ userId: decoded.userId });
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+});
+
+
+app.use('/api/auth/user', userRouter)
+app.use('/api/chats', chatRouter)
+
+io.on("connection", (socket) => {
+  // console.log('New Connection', socket.id)
+  socket.on('sendMessage', (data) => {
+    io.emit('receiveMessage', data)
+  })
+})
+
+server.listen(PORT, () => {
   console.log(`Server is running on Port ${PORT}`)
 })
